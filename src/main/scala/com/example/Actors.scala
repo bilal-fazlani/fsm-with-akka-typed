710d3dcb9a0869dc11fc529328ea9397f90a0ad6
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-08-02 22:48:47 +0530

    simplify Pritam's myReceive

--- src/main/scala/com/example/Actors.scala
+++ src/main/scala/com/example/Actors.scala
@@ -2,31 +2,37 @@ package com.example
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import com.example.Message.{ ReadBehaviorMessage, WriteBehaviorMessage }
 import com.example.Message.ReadBehaviorMessage.Read
 import com.example.Message.WriteBehaviorMessage.{ Clear, Save }
 import com.example.ReadResponse.Data
 import com.example.SaveResponse.Ok
 
+import scala.reflect.ClassTag
+
 object Actors {
-  def read(state: String): Behavior[Message] = Behaviors.receiveMessage {
+  def read(state: String): Behavior[Message] = myReceive[ReadBehaviorMessage] {
     case Read(replyTo) =>
       replyTo ! Data(state)
       write(state)
-    case m => unhandled(m)
   }
 
-  def write(state: String): Behavior[Message] = Behaviors.receiveMessage {
+  def write(state: String): Behavior[Message] = myReceive[WriteBehaviorMessage] {
     case Save(replyTo, value) =>
       replyTo ! Ok
       read(value)
     case Clear(replyTo) =>
       replyTo ! Ok
       read("")
-    case m => unhandled(m)
   }
 
   def unhandled(m: Message): Behavior[Message] = {
     m.replyTo ! Unhandled
     Behaviors.same
+  }
+
+  private def myReceive[B <: Message: ClassTag](f: B => Behavior[Message]): Behavior[Message] = Behaviors.receiveMessage {
+    case x: B => f(x)
+    case x => unhandled(x)
   }
 }
