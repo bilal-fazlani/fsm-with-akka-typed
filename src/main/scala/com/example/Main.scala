0106d64c273a2ff2fbe394944fb9b741bd3d1953
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-08-03 09:12:42 +0530

    general cleanup of routes etc

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -4,22 +4,22 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{ Failure, Success }
+import scala.util.{Failure, Success}
 
 object Main extends App with Directives with Routes {
 
-  val port = 8080
+  val port      = 8080
   val interface = "0.0.0.0"
 
   import Wiring._
 
   val f = for {
-    ref <- actorRef
-    _ = println("actor started")
-    proxy = new ActorProxy(ref)
+    ref    <- actorRef
+    _      = println("actor started")
+    proxy  = new ActorProxy(ref)
     routes = makeRoutes(proxy)
-    _ <- Http().bindAndHandle(routes, interface, port)
-    _ = println(s"http server started at $interface:$port")
+    _      <- Http().bindAndHandle(routes, interface, port)
+    _      = println(s"http server started at $interface:$port")
   } yield ()
 
   f.onComplete {
--- src/main/scala/com/example/Routes.scala
+++ src/main/scala/com/example/Routes.scala
@@ -2,54 +2,30 @@ package com.example
 
 import java.util.concurrent.TimeoutException
 
-import akka.http.scaladsl.model.HttpEntity.Strict
-import akka.http.scaladsl.model.{ContentTypes, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{Directives, ExceptionHandler, Route}
-import akka.util.ByteString
 import com.example.ReadBehaviorResponse.Data
 import com.example.WriteBehaviorResponse.Ok
 
 import scala.util.control.NonFatal
-import scala.util.{Failure, Success}
 
 trait Routes extends Directives {
-  private val exceptionHandler: ExceptionHandler = ExceptionHandler({
-    case _: TimeoutException =>
-      complete(HttpResponse(StatusCodes.GatewayTimeout))
-    case NonFatal(err) =>
-      complete(
-        HttpResponse(
-          StatusCodes.InternalServerError,
-          entity = Strict(
-            ContentTypes.`text/plain(UTF-8)`,
-            ByteString(
-              (err.getMessage :: "" :: err.getStackTrace
-                .map(x => x.toString)
-                .toList)
-                .mkString("\n")
-            )
-          )
-        )
-      )
-  })
+  private val exceptionHandler: ExceptionHandler = ExceptionHandler {
+    case _: TimeoutException => complete(StatusCodes.GatewayTimeout)
+    case NonFatal(err)       => complete(StatusCodes.InternalServerError -> err.getStackTrace.mkString("\n"))
+  }
 
-  def makeRoutes(proxy: ActorProxy): Route = {
-    handleExceptions(exceptionHandler) {
-      get {
-        onComplete(proxy.read) {
-          case Success(Data(value)) => complete(value)
-          case Success(Unhandled) =>
-            complete(HttpResponse(StatusCodes.BadRequest))
-          case Failure(exception) => throw exception
-        }
-      } ~
-      (post & parameter("data")) { data =>
-        onComplete(proxy.write(data)) {
-          case Success(Ok) => complete("")
-          case Success(Unhandled) =>
-            complete(HttpResponse(StatusCodes.BadRequest))
-          case Failure(exception) => throw exception
-        }
+  def makeRoutes(proxy: ActorProxy): Route = handleExceptions(exceptionHandler) {
+    get {
+      onSuccess(proxy.read) {
+        case Data(value) => complete(value)
+        case Unhandled   => complete(StatusCodes.BadRequest -> "")
+      }
+    } ~
+    (post & parameter("data")) { data =>
+      onSuccess(proxy.write(data)) {
+        case Ok        => complete("")
+        case Unhandled => complete(StatusCodes.BadRequest -> "")
       }
     }
   }
